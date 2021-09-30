4fefc996aa66de41788129ae4a8426086b5bbd66
Kaonan Micadei <k.micadei@gmail.com>
2021-09-30 23:25:27 +0200

    Add object parser

--- JsonParser.hs
+++ JsonParser.hs
@@ -4,7 +4,6 @@ import Parser
 import Data.Functor (($>))
 import Control.Applicative ((<|>), many)
 import Data.Char (isDigit)
-import Text.Read (readMaybe)
 
 data JValue
     = JNull
@@ -12,10 +11,11 @@ data JValue
     | JInt Int
     | JString String
     | JArray [JValue]
+    | JObject [(String, JValue)]
     deriving (Show)
 
 jvalue :: Parser JValue
-jvalue = jnull <|> jbool <|> jint <|> jstr <|> jarray
+jvalue = (jnull <|> jbool <|> jint <|> jstr <|> jarray <|> jobject) `surroundedBy` spaces
 
 jnull :: Parser JValue
 jnull = string "null" $> JNull
@@ -31,3 +31,8 @@ jstr = JString <$> literalString
 
 jarray :: Parser JValue
 jarray = JArray <$> list jvalue (char '[') (char ']') (char ',')
+
+jobject :: Parser JValue
+jobject = JObject <$> list pair (char '{') (char '}') (char ',')
+    where
+        pair = (,) <$> literalString <* (char ':' `surroundedBy` spaces) <*> jvalue
